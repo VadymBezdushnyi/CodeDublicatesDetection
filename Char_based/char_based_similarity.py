e473910943b221a3683d6c7786d1cd53232bddef
heimdall <vadim.bezdush@gmail.com>
2019-05-04 13:53:25 +0300

    add similarirty

---
+++ Char_based/char_based_similarity.py
@@ -0,0 +1,14 @@
+import textdistance
+import difflib
+
+class FileSimilarity:
+    def __init__(self, file1, file2):
+        self._file1 = file1
+        self._file2 = file2
+
+    def get_similarity(self):
+        lev = textdistance.levenshtein.normalized_similarity(self._file1, self._file2)
+        seq_match = difflib.SequenceMatcher(None, self._file1, self._file2).ratio()
+        return lev, seq_match
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from Char_based.char_based_similarity import *
 from antlr4 import *
 from antlr4.InputStream import InputStream
 
@@ -6,7 +7,7 @@ from Python3Lexer import Python3Lexer
 from Python3Parser import Python3Parser
 
 def printAst(parser:Python3Parser, node:RuleContext, indent:int = 0):
-    isSimplePath = node.getChildCount() == 1 and node.getChild(0).getChildCount() != 1
+    isSimplePath = node.getChildCount() == 1
     if not isSimplePath:
         for _ in range(indent):
             print(" ", end = "")
@@ -18,19 +19,31 @@ def printAst(parser:Python3Parser, node:RuleContext, indent:int = 0):
             printAst(parser, child, indent + (0 if isSimplePath else 1))
 
 
+def get_tokens(char_stream:str):
+    lexer = Python3Lexer(InputStream(char_stream))
+    all_tokens = lexer.getAllTokens()
+    return [i.text for i in all_tokens]
+
+
 
 if __name__ == '__main__':
-    s = open("test_example.py").read()
-    lexer = Python3Lexer(InputStream(s))
-    # all_tokens = lexer.getAllTokens()
-    # print([i.text for i in all_tokens])
+    s1 = open("test_example1.py").read()
+    # get_tokens(s1)
 
-    token_stream = CommonTokenStream(lexer)
-    parser = Python3Parser(token_stream)
-    # print([i.text for i in lexer.getAllTokens()])
+    s2 = open("test_example2.py").read()
+
+    tokens1 = get_tokens(s1)
+    tokens2 = get_tokens(s2)
+
+    fs_char = FileSimilarity(s1, s2)
+    fs_token = FileSimilarity(tokens1, tokens2)
+
+    print(fs_char.get_similarity())
+    print(fs_token.get_similarity())
+
     # tree = parser.expr().toStringTree()
     # print(tree)
-    printAst(parser, parser.file_input())
+    # printAst(parser, parser.file_input())
 
 
 
